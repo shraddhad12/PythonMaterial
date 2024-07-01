88341ccbb4f4423ad70f526257ec5231c74bcedd
Shraddha Dhotkar <dhotkarshraddha@gmail.com>
2024-07-01 10:30:26 +0530

    testing branch

---
+++ MyFolder/main5.py
@@ -0,0 +1,67 @@
+# class MyClass:
+
+#     count = 0
+    
+#     def __new__(self):
+#         if self.count == 0:
+#             print("object is creating")
+#             self.count += 1
+#             return MyClass()
+
+
+# obj = MyClass()
+# obj = MyClass()
+
+# A = [(1, 1) , (1, 2) , (2, 2), (2, 1) (4, 2), (4, 3), (5, 3)]
+
+# x = 0
+# y = 0
+
+# for tuple in A:
+
+A = [[1, 2, 3], 
+     [4, 5, 6], 
+     [7, 8, 9]]
+
+#0 = [1,2,3,6,9,8,7,4,5]
+
+# #[1,2,3,6,5,4,9,8,7]
+
+# row = len(A)
+# column = len(A[0])
+# print(row, column)
+# x = 0
+# y = 0
+# output = []
+# for i in range(len(A)):
+#     for j in range(len(A[0])):
+#         print(A[i][j])
+#         output.append(A[i][j])
+        
+
+
+
+# print(output)
+
+def api(params):
+    pass
+
+# context manager
+# memory management in python
+# how can we create api without using framework
+# is for look generator or iterator
+# create class without using class keyword
+# singleton object
+# reference count in GC
+# is python call by value or call by refernce
+
+
+
+
+
+
+
+
+
+    
+
